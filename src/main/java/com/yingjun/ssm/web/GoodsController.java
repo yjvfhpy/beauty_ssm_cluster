d32da67c9f7ba303c0fd7546318560ceda83a575
micalliu <382796497@qq.com>
2017-12-28 16:06:47 +0800

    配置文件修改

--- src/main/java/com/yingjun/ssm/web/GoodsController.java
+++ src/main/java/com/yingjun/ssm/web/GoodsController.java
@@ -29,15 +29,7 @@ public class GoodsController {
 	@Autowired
 	private GoodsService goodsService;
 
-	/*
-	 * @RequestMapping(value = "/list", method = RequestMethod.GET) public
-	 * String list(Model entity, Integer offset, Integer limit) {
-	 * logger.info("invoke----------/goods/list"); offset = offset == null ? 0 :
-	 * offset;//默认便宜0 limit = limit == null ? 50 : limit;//默认展示50条 List<Goods>
-	 * list = goodsService.getGoodsList(offset, limit);
-	 * entity.addAttribute("goodslist", list); return "goodslist"; }
-	 */
-
+	
 	/**
 	 * 摒弃jsp页面通过ajax接口做到真正意义上的前后分离
 	 * 
